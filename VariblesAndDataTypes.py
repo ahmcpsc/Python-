1cf9c650ca8e1879ba8df7b51a3470bf7f28ceee
ahmcpsc <81540015+ahmcpsc@users.noreply.github.com>
2021-03-29 20:07:07 -0600

    Add files via upload

---
+++ VariblesAndDataTypes.py
@@ -0,0 +1,21 @@
+#variables and data types:
+
+#a variable is a container to store some data values
+#to create a variable: name of a variable (assign a name to this container)
+#tne assign a value to it
+
+student_name = "Ali"    #assign the value Ali to the variable named student_name or
+                        # store the value Ali iside the variable named student_name
+student_age = "20"      #assign the value 20 to the variable name student_age.
+print("The student name is "+student_name+" and his age is "+student_age+".")
+
+#data types:
+is_male = True #bolean
+name= "John"   #sting
+age = 53       #integer
+
+
+
+student_age = "50"      #assign the value 50 to the variable name student_age(updated the value).
+print("The student name is "+student_name+" and his age is "+student_age+".")
+
