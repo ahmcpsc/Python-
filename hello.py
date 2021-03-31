750907c565f279dcc4b1a851c7766d6232846207
ahmcpsc <81540015+ahmcpsc@users.noreply.github.com>
2021-03-31 14:26:07 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,5 @@
+# programing is list of instructions given to the computer to follow
+# After you click save and run the following happens:
+# 1. python interpreter looks at what is in the source code (set of instructions) and
+# 2. prints out whatever relevant output
+print("hello Ahmed")
